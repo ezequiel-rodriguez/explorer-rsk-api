3b42ffb23162d4b9f2463938c880b3c9e8c8794c
ivegabr <isidro.vega@iovlabs.org>
2025-02-20 19:07:36 +0100

    remove unused injected service

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -2,16 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PrismaService } from 'src/prisma.service';
-import { PaginationService } from 'src/common/pagination/pagination.service';
 import { TxParserService } from 'src/common/parsers/transaction-parser.service';
 
 @Module({
-  providers: [
-    TransactionsService,
-    PrismaService,
-    PaginationService,
-    TxParserService,
-  ],
+  providers: [TransactionsService, PrismaService, TxParserService],
   controllers: [TransactionsController],
 })
 export class TransactionsModule {}
