c56159eb2156bf795705a9fcdff10826748476ee
ivegabr <isidro.vega@iovlabs.org>
2025-02-21 18:50:51 +0100

    remove repeated property

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -120,8 +120,6 @@ export class EventsService {
 
       const formattedData = paginatedEvents.map((e) => {
         e.timestamp = e.timestamp.toString() as unknown as bigint;
-        e.abi = JSON.parse(e.abi);
-        e.args = JSON.parse(e.args);
         const abi = JSON.parse(e.abi);
         let args = JSON.parse(e.args);
         args = abi?.inputs?.map((input, index) => {
