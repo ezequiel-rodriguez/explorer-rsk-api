bbbb5ba56a5c492d9108174f6a407fe17c34e71e
ivegabr <isidro.vega@iovlabs.org>
2025-02-23 01:51:29 +0100

    add cursorValidationPipe

---
+++ src/common/pipes/cursor-validation.pipe.ts
@@ -0,0 +1,89 @@
+import { PipeTransform, BadRequestException, Injectable } from '@nestjs/common';
+import { MAX_INT_4_BYTES } from '../constants';
+
+@Injectable()
+export class CursorValidationPipe implements PipeTransform {
+  constructor(
+    private expectedFormat:
+      | 'address_blockNumber'
+      | 'blockNumber_transactionIndex',
+  ) {}
+
+  transform(value: string): { [key: string]: string | number } {
+    if (!value) {
+      return undefined;
+    }
+
+    let parts: string[];
+
+    if (this.expectedFormat === 'address_blockNumber') {
+      parts = value.split('_');
+
+      console.log(parts);
+
+      if (parts.length !== 2) {
+        throw new BadRequestException(
+          `Invalid cursor format. Expected format: "address_blockNumber". Received: "${value}".`,
+        );
+      }
+
+      const [address, blockNumber] = parts;
+
+      if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
+        throw new BadRequestException(
+          `Invalid address format in cursor: ${address}`,
+        );
+      }
+
+      const parsedBlockNumber = parseInt(blockNumber, 10);
+      if (isNaN(parsedBlockNumber) || parsedBlockNumber < 0) {
+        throw new BadRequestException(
+          `Invalid blockNumber in cursor: ${blockNumber}`,
+        );
+      }
+
+      if (parsedBlockNumber > MAX_INT_4_BYTES) {
+        throw new BadRequestException(
+          `blockNumber in cursor exceeds max allowed value: ${blockNumber}`,
+        );
+      }
+
+      return { address, blockNumber: parsedBlockNumber };
+    }
+
+    if (this.expectedFormat === 'blockNumber_transactionIndex') {
+      parts = value.split('_');
+      if (parts.length !== 2) {
+        throw new BadRequestException(
+          `Invalid cursor format. Expected format: "blockNumber_transactionIndex". Received: "${value}".`,
+        );
+      }
+
+      const [blockNumber, transactionIndex] = parts;
+
+      const parsedBlockNumber = parseInt(blockNumber, 10);
+      const parsedTransactionIndex = parseInt(transactionIndex, 10);
+
+      if (isNaN(parsedBlockNumber) || parsedBlockNumber < 0) {
+        throw new BadRequestException(
+          `Invalid blockNumber in cursor: ${blockNumber}`,
+        );
+      }
+
+      if (isNaN(parsedTransactionIndex) || parsedTransactionIndex < 0) {
+        throw new BadRequestException(
+          `Invalid transactionIndex in cursor: ${transactionIndex}`,
+        );
+      }
+
+      return {
+        blockNumber: parsedBlockNumber,
+        transactionIndex: parsedTransactionIndex,
+      };
+    }
+
+    throw new BadRequestException(
+      `Unknown cursor format: ${this.expectedFormat}`,
+    );
+  }
+}
